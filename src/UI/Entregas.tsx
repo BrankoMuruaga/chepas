4f190d43ab9853222367fc99d0f498161edaa1c7
BrankoMuruaga <muruagabranko253@gmail.com>
2025-04-27 15:44:57 -0300

    feat: agregar nuevo SVG para la línea de retiro móvil y ajustar estilos de scrollbar

--- src/UI/Entregas.tsx
+++ src/UI/Entregas.tsx
@@ -12,13 +12,14 @@ function Entregas({ id }: { id: string }) {
       >
         <section className="w-full md:w-3/4 flex flex-col md:flex-row justify-around items-center md:m-10 gap-10">
           {envios.map((envio) => (
-            <SectionDashed
-              className="md:w-1/2 w-96 h-96 py-14 px-5 md:p-14"
-              key={envio.name}
-            >
-              <img className="h-44" src={envio.image} alt="" />
+            <SectionDashed className="md:w-1/2 h-96 p-14" key={envio.name}>
+              <img
+                className="h-44 max-h-44 object-contain"
+                src={envio.image}
+                alt=""
+              />
               <h1 className="text-title text-3xl">{envio.name}</h1>
-              <p className="text-subtitle text-lg md:text-2xl text-center">
+              <p className="text-subtitle text-lg md:text-2xl w-3/4 text-center">
                 {envio.descripcion}
               </p>
             </SectionDashed>
@@ -27,7 +28,11 @@ function Entregas({ id }: { id: string }) {
       </Section>
       <img
         src="/images/linea-pagos.svg"
-        className="bottom-7/12 -rotate-12 translate-y-24 pointer-events-none absolute -z-10 w-screen"
+        className="bottom-7/12 -rotate-12 translate-y-24 pointer-events-none absolute -z-10 w-screen hidden md:block"
+      />
+      <img
+        src="/images/linea-retiro-mobile.svg"
+        className="bottom-7/12  translate-y-24 pointer-events-none absolute -z-10 w-screen md:hidden"
       />
     </>
   );
