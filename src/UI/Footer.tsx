bcc74d74edf00c20d4acae063ad281f4f51d4e41
BrankoMuruaga <muruagabranko253@gmail.com>
2025-04-27 21:11:42 -0300

    Refactor Footer component: remove duplicate Instagram import, add lazy loading for wave image, and introduce Logo section for larger screens

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -1,6 +1,7 @@
+import Instagram from "../icons/Instagram";
 import Logo from "../components/Logo";
 import Wave from "../components/Wave";
-import Instagram from "../icons/Instagram";
+import { navbarItems } from "../utils/data";
 
 function Footer() {
   return (
@@ -15,6 +16,9 @@ function Footer() {
         src="/images/wave-footer-mobile.svg"
         className="md:hidden absolute -z-10 bottom-0 left-0 w-full overflow-hidden"
       />
+      <section className=" w-full justify-start items-start top-32 h-20 pl-24 hidden md:flex">
+        <Logo className="h-24 top-0 -translate-y-10  absolute"></Logo>
+      </section>
 
       <section className="flex flex-col md:flex-row justify-center md:justify-between items-center px-4 pb-2 mt-24 gap-5 md:gap-0 md:mt-0">
         <a
